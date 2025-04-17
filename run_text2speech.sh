8b220efe29b8dfaebaed6ab7e26ef9fcc4e5ff57
Juloos <juloos.sfr@gmail.com>
2025-04-17 10:00:17 +0200

    ipv4 in launch script param

--- run_text2speech.sh
+++ run_text2speech.sh
@@ -2,10 +2,14 @@
 
 cd "$(dirname "$0")"
 
+if [ -z "$1" ]; then
+  echo "Usage: $0 <ipv4>"
+  exit 1
+fi
+
 ./bin/wsk property set --apihost "http://172.17.0.1:3233" --auth "23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP"
 ./bin/wskdeploy -m ./text2speech/manifest.yaml
 
-IPV4="172.17.0.1"
 SCHEMAS=("S1" "S2" "S3" "S4" "S5")
 TEXTES=("1Ko.txt" "5Ko.txt" "12Ko.txt")
 
@@ -15,7 +19,7 @@ for SCHEMA in "${SCHEMAS[@]}"; do
     echo -e "Starting $SCHEMA with $TEXT at $(date)"
     for (( i = 0; i < 101 ; i++ )); do
       ./bin/wsk action invoke "demo/$SCHEMA" -r \
-        --param ipv4 "$IPV4" \
+        --param ipv4 "$1" \
         --param schema "$SCHEMA" \
         --param text "$TEXT"
     done
