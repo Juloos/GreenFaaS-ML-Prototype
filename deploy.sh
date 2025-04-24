b7d9cce611245cbdcede6afc85518904c5160729
Juloos <juloos.sfr@gmail.com>
2025-04-24 14:58:33 +0200

    debug 10

--- deploy.sh
+++ deploy.sh
@@ -12,13 +12,13 @@ fi
 echo "Updating git..."
 for HOST in $HOSTS; do
   echo "  on $HOST"
-  ssh root@$HOST "cd GreenFaaS-ML-Prototype ; git pull" 2>/dev/null >/dev/null
+  ssh root@$HOST "cd GreenFaaS-ML-Prototype ; git pull" >/dev/null 2>&1
 done
 
 echo "Launching OpenWhisk..."
 for HOST in $HOSTS; do
   echo "  on $HOST"
-  ssh root@$HOST "nohup ./GreenFaaS-ML-Prototype/run_openwhisk.sh </dev/null &" 2>/dev/null >/dev/null &
+  ssh root@$HOST "nohup ./GreenFaaS-ML-Prototype/run_openwhisk.sh </dev/null &" >/dev/null 2>&1 &
 done
 
 echo "Waiting a bit to make sure everything is up and running..."
@@ -27,19 +27,22 @@ sleep 30s
 IPV4=`cat .ipv4`
 echo "Using ipv4=$IPV4"
 
+mkdir -p logs
 echo "Deploying the demo..."
 for HOST in $HOSTS; do
   echo "  on $HOST"
-  ssh root@$HOST "./GreenFaaS-ML-Prototype/run_text2speech.sh '$IPV4'" 2>/dev/null >/dev/null && \
+  ssh root@$HOST "./GreenFaaS-ML-Prototype/run_text2speech.sh '$IPV4' >tts.log 2>&1" >/dev/null 2>&1 && \
     scp -r root@$HOST:/root/GreenFaaS-ML-Prototype/energy_results . && \
-    touch "energy_results/${HOST}_done" &
+    scp root@$HOST:/root/tts.log logs/$HOST.log &
 done
 
 echo "Waiting for all hosts to finish..."
-for HOST in $HOSTS; do
-  while [ ! -f "energy_results/${HOST}_done" ]; do
-    sleep 10s
+while [ -n "$HOSTS" ]; do
+  for HOST in $HOSTS; do
+    if [ -f "logs/$HOST.log" ]; then
+      echo "  on $HOST: done"
+      HOSTS=${HOSTS//$HOST/}
+    fi
   done
-  echo "  on $HOST: done"
-  rm "energy_results/${HOST}_done"
+  sleep 10s
 done
