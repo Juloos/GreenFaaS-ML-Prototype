e898a22aa016f21b76ea68e9b8ecb3e34588ac67
Juloos <juloos.sfr@gmail.com>
2025-05-07 16:56:03 +0200

    ml scripts

--- decision_tree/data.py
+++ decision_tree/data.py
@@ -1,6 +1,7 @@
 import csv
 import os
 from random import shuffle
+from typing import List, Tuple
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
@@ -13,15 +14,23 @@ with open("../energy_results/all.csv", "r") as f:
     for row in reader:
         if not row:
             continue
-        schema = int(row[0].removeprefix('S'))
+        schema = int(row[0].strip().removeprefix('S'))
         schemas.add(schema)
-        text = int(row[1].removesuffix("Ko.txt"))  # Should be done better in prod, with file transfer detection for example
-        watts = float(row[2])
-        milliseconds = float(row[3])
+        text = int(row[1].strip().removesuffix("Ko.txt"))  # Should be done better in prod, with file transfer detection for example
+        watts = float(row[2].strip())
+        milliseconds = float(row[3].strip())
         DATA.append((schema, text, watts, milliseconds))
 
-shuffled_schemas = list(schemas)
-shuffle(shuffled_schemas)
-print("Shufle : " + str(shuffled_schemas))
-RAMBLE = {x: y for x, y in zip(schemas, shuffled_schemas)}
-UNRAMBLE = {y: x for x, y in zip(schemas, shuffled_schemas)}
+total_watts = {schema: sum(row[2] for row in DATA if row[0] == schema) for schema in schemas}
+sorted_schemas = sorted(schemas, key=lambda x: total_watts[x])
+sorted_schemas_mapping = {sa: sb for sa, sb in zip(schemas, sorted_schemas)}
+SORTSCHEMA_DATA = [(sorted_schemas_mapping[row[0]], *row[1:]) for row in DATA]
+
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# Create a 3D graph of the given domain
+def plot_domain(ax: plt.Axes, data: List[Tuple[int, int, float]], color: str):
+    Axes3D.scatter(ax, [row[0] for row in data], [row[1] for row in data], [row[2] for row in data], 'o', color=color)
+
--- decision_tree/main.py
+++ decision_tree/main.py
@@ -1,30 +1,72 @@
-from data import DATA
+from data import DATA, SORTSCHEMA_DATA, plot_domain
 
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
+from sklearn import tree
 from random import randint
+import matplotlib.pyplot as plt
 
 SCHEMA, TEXT, WATTS, MS = range(4)
 
-step = 10000  # classes are intervals of size <step> watts
-X = [(row[SCHEMA], row[TEXT]) for row in DATA]
-Y = [int(row[WATTS] // step) for row in DATA]
+setseed = None  # Replace with your seed for reproducibility
+fitsNumber = 1000  # Number of fits to do
+n_estimators = 100  # Number of trees in the forest
+step = 1  # classes are intervals of size <step> watts, not useful for regression (set to 1)
 
-print("X : " + str(X))
-print("Y : " + str(Y))
+data = SORTSCHEMA_DATA
+X = [(row[SCHEMA], row[TEXT]) for row in data]
+Y = [int(row[WATTS] // step) for row in data]
 
-seed = randint(0, 1000000)
-print("Seed : " + str(seed))
-xTrain, xTest, yTrain, yTest = train_test_split(X, Y, test_size=0.2, random_state=seed)
+# print("X : " + str(X))
+# print("Y : " + str(Y))
 
-print("Train : " + str(xTrain))
-print("Test : " + str(xTest))
-print("Train Y : " + str(yTrain))
-print("Test Y : " + str(yTest))
+def show_decitree_data(decitree, xTrain, xTest):
+    predictions = decitree.predict(xTest)
 
-dcl = DecisionTreeClassifier(random_state=seed)
-print("Training...")
-dcl.fit(xTrain, yTrain)
-print("Score : " + str(dcl.score(xTest, yTest)))
-for i, x in enumerate(xTest):
-    print("  " + str(x) + " -> " + str(dcl.predict([x])[0]) + " vs " + str(yTest[i]))
+    # Show the difference between predicted and real values
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.set_xlabel("Schema")
+    ax.set_ylabel("Text KB")
+    ax.set_zlabel(("Watts Interval (%d)" % step) if step > 1 else "Watts")
+    plot_domain(ax, [(row[SCHEMA], row[TEXT], int(row[WATTS] // step)) for row in data], "red")
+    plot_domain(ax, [(*row, decitree.predict([row])[0]) for row in (xTrain)], "yellow")
+    plot_domain(ax, [(*row, y) for row, y in zip(xTest, predictions)], "deepskyblue")
+    plt.show()
+
+    # Show the decision tree's parameters
+    tree.plot_tree(decitree)
+    plt.savefig("tree.png", dpi=500)
+
+min_score, max_score = 1, 0
+min_seed, max_seed = 0, 0
+total_score = 0
+for i in range(fitsNumber):
+    seed = setseed or randint(0, 4294967295)
+    xTrain, xTest, yTrain, yTest = train_test_split(X, Y, test_size=0.2, random_state=seed)
+    decitree = RandomForestRegressor(random_state=seed, n_estimators=n_estimators)
+
+    print("Training... (%d/%d)" % (i + 1, fitsNumber), end='\r')
+    decitree.fit(xTrain, yTrain)
+    if setseed is not None:
+        print("Training done")
+        show_decitree_data(decitree, xTrain, xTest)
+        exit(0)
+
+    score = decitree.score(xTest, yTest)
+    # print("Score for seed %d : %.5f" % (seed, score))
+    total_score += score
+    if score < min_score:
+        min_score = score
+        min_seed = seed
+    if score > max_score:
+        max_score = score
+        max_seed = seed
+
+print("Min score : %.5f with seed %d" % (min_score, min_seed))
+print("Max score : %.5f with seed %d" % (max_score, max_seed))
+print("Average score : %.5f" % (total_score / fitsNumber))
+
+# for i, x in enumerate(xTest):
+#     print("  " + str(x) + " -> " + str(decitree.predict([x])[0]) + " vs " + str(yTest[i]))
