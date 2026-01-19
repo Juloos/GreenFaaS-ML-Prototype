cd6570b07070d0f80403271c4f5a45bc28993215
Juloos <juloos.sfr@gmail.com>
2026-01-19 15:05:53 +0100

    debug 12

--- text2speech/speech/__main__.py
+++ text2speech/speech/__main__.py
@@ -49,7 +49,7 @@ def push(obj, ipv4):
 def espeakSpeech(file, ttsid):
     with open(file, 'r') as f:
         textSize = len(f.read())
-    command = f'espeak-ng -f {file} --stdout | ffmpeg -y -i pipe: {ttsid}.wav -loglevel quiet'
+    command = f'espeak-ng -f {file} -w {ttsid}.wav'
     # Run the command in the shell
     subprocess.run(command, shell=True, check=True)
     return f"{ttsid}.wav", textSize
