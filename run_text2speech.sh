36d98ec10d96dfa82eee178da8434cbbdf73fc05
Juloos <juloos.sfr@gmail.com>
2026-01-20 14:39:47 +0100

    debug 4

--- run_text2speech.sh
+++ run_text2speech.sh
@@ -15,7 +15,7 @@ else
   ITERATIONS=$2
 fi
 
-./bin/wsk -i property set --apihost "http://localhost:31001" --auth "23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP"
+./bin/wsk -i property set --apihost "https://localhost:31001" --auth "23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP"
 ./bin/wskdeploy -m text2speech/manifest.yaml || 
   { echo "Failed to deploy, make sure OpenWhisk is running."; exit 1; }
 
