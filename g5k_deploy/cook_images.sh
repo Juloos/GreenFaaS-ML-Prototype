8f982ee3253ca69e239b4318bf9a5e0aa1ea60e1
Juloos <juloos.sfr@gmail.com>
2026-01-22 13:51:45 +0100

    debug 7

--- g5k_deploy/cook_images.sh
+++ g5k_deploy/cook_images.sh
@@ -28,7 +28,7 @@ ssh root@$HOST <<-"EOF" |& sed "s/^/  /"
   install -m 0755 -d /etc/apt/keyrings |& sed "s/^/  /"
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc |& sed "s/^/  /"
   chmod a+r /etc/apt/keyrings/docker.asc |& sed "s/^/  /"
-  tee /etc/apt/sources.list.d/docker.sources <<-EOF2 |& sed "s/^/  /"
+  tee /etc/apt/sources.list.d/docker.sources <<-\ \ EOF2 |& sed "s/^/  /"
     Types: deb
     URIs: https://download.docker.com/linux/ubuntu
     Suites: $(. /etc/os-release && echo -e "\n${UBUNTU_CODENAME:-$VERSION_CODENAME}")
