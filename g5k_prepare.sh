24dd9dd9c978bb11db1913f2e58889808af7353c
Juloos <juloos.sfr@gmail.com>
2026-01-22 12:06:49 +0100

    debug 3

--- g5k_prepare.sh
+++ g5k_prepare.sh
@@ -4,8 +4,9 @@ cd "$(dirname "$0")"
 
 
 echo "Forwarding public files"
-cp -r ./g5k_deploy/public/* ~/public/ | sed 's/^/  /'
+cp -r ./g5k_deploy/public/* ~/public/ |& sed 's/^/  /'
 
 echo "Submitting the image cooking job"
-oarsub -S ./g5k_deploy/cook_images.sh -O g5k_image_cooking.log -E g5k_image_cooking.log | sed 's/^/  /'
+rm g5k_image_cooking*.log
+oarsub -S ./g5k_deploy/cook_images.sh -O g5k_image_cooking.log -E g5k_image_cooking.err.log |& sed 's/^/  /'
 watch -c -t -n 0.1 cat g5k_image_cooking.log
