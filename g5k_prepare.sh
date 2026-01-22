97faf2e7325ecb3dd29a41a9c22f08531cca8bfb
Juloos <juloos.sfr@gmail.com>
2026-01-22 17:44:34 +0100

    wtf

--- g5k_prepare.sh
+++ g5k_prepare.sh
@@ -17,7 +17,7 @@ rm g5k_image_cooking*.log
 
 cp -r ./g5k_deploy/public/* ~/public/
 for config_file in $(basename -a ./g5k_deploy/public/*.yml); do
-  sed "s/{{USER}}/$USER/g" -i "~/public/$config_file"
+  sed "s/{{USER}}/$USER/g" -i ~/public/$config_file
 done
 
 oarsub -S ./g5k_deploy/cook_images.sh -O g5k_image_cooking.log -E g5k_image_cooking.err.log >g5k_image_cooking.log
