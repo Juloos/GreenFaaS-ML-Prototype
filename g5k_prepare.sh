4df72084a77e2d703947d7d56af2a470ca3772a0
Juloos <juloos.sfr@gmail.com>
2026-01-22 14:29:50 +0100

    debug 11

--- g5k_prepare.sh
+++ g5k_prepare.sh
@@ -3,10 +3,11 @@
 cd "$(dirname "$0")"
 
 
+rm g5k_image_cooking*.log
+
 echo "Forwarding public files"
 cp -r ./g5k_deploy/public/* ~/public/ |& sed 's/^/  /'
 
 echo "Submitting the image cooking job"
-rm g5k_image_cooking*.log
 oarsub -S ./g5k_deploy/cook_images.sh -O g5k_image_cooking.log -E g5k_image_cooking.err.log |& sed 's/^/  /'
-vim -R --cmd "set autoread" g5k_image_cooking.log
+vim -R --cmd 'set autoread | set updatetime=100 | au CursorHold * checktime | call feedkeys("G")' g5k_image_cooking.log +
