d24fd241161595bec395be65e5daa68c933adb16
Juloos <juloos.sfr@gmail.com>
2026-01-22 15:13:31 +0100

    debug 17

--- g5k_prepare.sh
+++ g5k_prepare.sh
@@ -3,11 +3,10 @@
 cd "$(dirname "$0")"
 
 
+rm ~/public/openwhisk_kube_*.tar.zst ~/public/docker_swift_*.tar.zst
 rm g5k_image_cooking*.log
 
-echo "Forwarding public files"
 cp -r ./g5k_deploy/public/* ~/public/
 
-echo "Submitting the image cooking job"
 oarsub -S ./g5k_deploy/cook_images.sh -O g5k_image_cooking.log -E g5k_image_cooking.err.log >g5k_image_cooking.log
-watch -c -t -n 1 'cat g5k_image_cooking.log | tail -32'
+view g5k_image_cooking.log
