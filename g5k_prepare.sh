0503239ab8bf70b7afe7858e9611857e591f936a
Juloos <juloos.sfr@gmail.com>
2026-01-22 17:39:53 +0100

    debug 6

--- g5k_prepare.sh
+++ g5k_prepare.sh
@@ -16,6 +16,9 @@ rm ~/public/openwhisk_kube_*.tar.zst ~/public/docker_swift_*.tar.zst
 rm g5k_image_cooking*.log
 
 cp -r ./g5k_deploy/public/* ~/public/
+for config_file in $(ls ./g5k_deploy/public/*.yml); do
+  sed "s/{{USER}}/$USER/g" -i "~/public/$config_file"
+done
 
 oarsub -S ./g5k_deploy/cook_images.sh -O g5k_image_cooking.log -E g5k_image_cooking.err.log >g5k_image_cooking.log
 view g5k_image_cooking.log
