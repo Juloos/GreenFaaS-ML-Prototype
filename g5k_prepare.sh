310c551186d8c25348d1500dcc258bd577ab436a
Juloos <juloos.sfr@gmail.com>
2026-01-22 14:54:34 +0100

    debug 14

--- g5k_prepare.sh
+++ g5k_prepare.sh
@@ -10,4 +10,4 @@ cp -r ./g5k_deploy/public/* ~/public/
 
 echo "Submitting the image cooking job"
 oarsub -S ./g5k_deploy/cook_images.sh -O g5k_image_cooking.log -E g5k_image_cooking.err.log >g5k_image_cooking.log
-vim -R --cmd 'set autoread | set updatetime=100 | au CursorHold * checktime | call feedkeys("G")' g5k_image_cooking.log +
+vim -R --cmd 'set mouse=a | syntax on | set autoread | set updatetime=100 | au CursorHold * checktime | call feedkeys("G")' g5k_image_cooking.log +
