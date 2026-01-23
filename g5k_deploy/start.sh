e30f9e2a425708d862d72b470646f4d904274707
Juloos <juloos.sfr@gmail.com>
2026-01-23 14:56:06 +0100

    debug 10

--- g5k_deploy/start.sh
+++ g5k_deploy/start.sh
@@ -23,8 +23,11 @@ HOSTS=`oarprint host -P host,cluster,core_count,memnode,wattmeter`
 echo "Selecting resources from"
 echo "  host cluster core_count memnode wattmeter"
 echo -e "$HOSTS" | sed "s/^/  /"
-echo "With NB_OW=$NB_OW"
-echo "     PWD=$PWD"
+echo "With"
+echo "  MD5=$MD5"
+echo "  SCRIPTPATH=$SCRIPTPATH"
+echo "  PWD=$PWD"
+echo "  NB_OW=$NB_OW"
 
 mapfile -t OW_ELIGIBLE < <(
   echo -e $HOSTS | awk '$3 >= 12 && $4 >= 16384 && $5 == "YES"'
