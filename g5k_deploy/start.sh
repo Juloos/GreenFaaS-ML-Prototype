08fc19adf669ae7eb5501bdf654bf14b7ae5f80a
Juloos <juloos.sfr@gmail.com>
2026-01-23 15:02:53 +0100

    SINGE

--- g5k_deploy/start.sh
+++ g5k_deploy/start.sh
@@ -3,7 +3,7 @@
 
 MD5="$(md5sum "$0" | cut -d ' ' -f 1)"
 SCRIPTPATH="$(realpath "$0")"
-cd "$(dirname "$SCRIPTDIR")/.."
+cd "$(dirname "$0")"
 git pull
 [ "$MD5" == "$(md5sum "$SCRIPTPATH" | cut -d ' ' -f 1)" ] || {
   bash "$SCRIPTPATH"
