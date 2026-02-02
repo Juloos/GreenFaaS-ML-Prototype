6201058d5cc3297cf1b23349236f8a71d4cfbb31
Juloos <juloos.sfr@gmail.com>
2026-02-02 16:03:19 +0100

    separate using job id

--- g5k_deploy/start.sh
+++ g5k_deploy/start.sh
@@ -42,15 +42,15 @@ echo "Deploying the demo..."
 for HOST in "${OW_HOSTS[@]}"; do
   echo "  on $HOST"
   ssh root@$HOST "./greenfaas/g5k_deploy/run_text2speech.sh '$(IFS=','; echo "${SWIFT_HOSTS[*]}")' '$ITERATIONS' '$RUNS' >tts.log 2>&1" >/dev/null 2>&1 &&
-    scp -r root@$HOST:/root/greenfaas/energy_results . &&
-    scp root@$HOST:/root/tts.log logs/$HOST.log &
+    scp -r root@$HOST:/root/greenfaas/energy_results/*/ ./energy_results/$OAR_JOB_ID/ &&
+    scp root@$HOST:/root/tts.log logs/$HOST.$OAR_JOB_ID.log &
   shift_left SWIFT_HOSTS
 done
 
 echo "Waiting for tasks to finish..."
 while [ -n "$(tr -d ' ' <<<"${OW_HOSTS[@]}")" ]; do
   for HOST in "${OW_HOSTS[@]}"; do
-    if [ -f "logs/$HOST.log" ]; then
+    if [ -f "logs/$HOST.$OAR_JOB_ID.log" ]; then
       echo "  on $HOST: done"
       OW_HOSTS=("${OW_HOSTS[@]/$HOST}")
     fi
