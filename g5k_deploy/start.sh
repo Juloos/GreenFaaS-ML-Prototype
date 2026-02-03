663e49ff22b3dfe857bac55f70632c8ee56ab626
Juloos <juloos.sfr@gmail.com>
2026-02-03 13:48:10 +0100

    debug 2

--- g5k_deploy/start.sh
+++ g5k_deploy/start.sh
@@ -41,9 +41,10 @@ fi
 echo "Deploying the demo..."
 for HOST in "${OW_HOSTS[@]}"; do
   echo "  on $HOST"
+  mkdir -p ./energy_results/$OAR_JOB_ID
   HHOSTNAME="$(cut -d '.' -f 1 <<<"$HOST")"
   ssh root@$HOST "./greenfaas/g5k_deploy/run_text2speech.sh '$(IFS=','; echo "${SWIFT_HOSTS[*]}")' '$ITERATIONS' '$RUNS' >tts.log 2>&1" >/dev/null 2>&1 &&
-    scp -r root@$HOST:/root/greenfaas/energy_results/$HHOSTNAME ./energy_results/$OAR_JOB_ID/$HHOSTNAME &&
+    scp -r root@$HOST:/root/greenfaas/energy_results/$HHOSTNAME ./energy_results/$OAR_JOB_ID &&
     scp root@$HOST:/root/tts.log logs/$HHOSTNAME.$OAR_JOB_ID.log &
   shift_left SWIFT_HOSTS
 done
