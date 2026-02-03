9593dd7e84c4fd7ac2df03e0b228848f81812c13
Juloos <juloos.sfr@gmail.com>
2026-02-03 10:46:49 +0100

    debug 1

--- g5k_deploy/start.sh
+++ g5k_deploy/start.sh
@@ -41,16 +41,18 @@ fi
 echo "Deploying the demo..."
 for HOST in "${OW_HOSTS[@]}"; do
   echo "  on $HOST"
+  HHOSTNAME="$(cut -d '.' -f 1 <<<"$HOST")"
   ssh root@$HOST "./greenfaas/g5k_deploy/run_text2speech.sh '$(IFS=','; echo "${SWIFT_HOSTS[*]}")' '$ITERATIONS' '$RUNS' >tts.log 2>&1" >/dev/null 2>&1 &&
-    scp -r root@$HOST:/root/greenfaas/energy_results/*/ ./energy_results/$OAR_JOB_ID/ &&
-    scp root@$HOST:/root/tts.log logs/$HOST.$OAR_JOB_ID.log &
+    scp -r root@$HOST:/root/greenfaas/energy_results/$HHOSTNAME ./energy_results/$OAR_JOB_ID/$HHOSTNAME &&
+    scp root@$HOST:/root/tts.log logs/$HHOSTNAME.$OAR_JOB_ID.log &
   shift_left SWIFT_HOSTS
 done
 
 echo "Waiting for tasks to finish..."
 while [ -n "$(tr -d ' ' <<<"${OW_HOSTS[@]}")" ]; do
   for HOST in "${OW_HOSTS[@]}"; do
-    if [ -f "logs/$HOST.$OAR_JOB_ID.log" ]; then
+    HHOSTNAME="$(cut -d '.' -f 1 <<<"$HOST")"
+    if [ -f "logs/$HHOSTNAME.$OAR_JOB_ID.log" ]; then
       echo "  on $HOST: done"
       OW_HOSTS=("${OW_HOSTS[@]/$HOST}")
     fi
