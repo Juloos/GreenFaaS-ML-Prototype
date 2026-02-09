86a3e6f5702ccf911f9eaa07989eef57963b1925
Juloos <juloos.sfr@gmail.com>
2026-02-09 21:40:19 +0100

    debug 8

--- g5k_deploy/start.sh
+++ g5k_deploy/start.sh
@@ -42,12 +42,7 @@ echo "Deploying the demo..."
 for HOST in "${OW_HOSTS[@]}"; do
   echo "  on $HOST"
   mkdir -p ./energy_results/${ITERATIONS}i-${RUNS}r-${1}ow_${OAR_JOB_ID}
-  HHOSTNAME="$(cut -d '.' -f 1 <<<"$HOST")"
-  ( 
-    ssh -o StrictHostKeyChecking=no root@$HOST "./greenfaas/g5k_deploy/run_text2speech.sh '$(IFS=','; echo "${SWIFT_HOSTS[*]}")' '$ITERATIONS' '$RUNS' >tts.log 2>&1" >/dev/null 2>&1 &&
-    scp -r root@$HOST:/root/greenfaas/energy_results/$HHOSTNAME ./energy_results/${ITERATIONS}i-${RUNS}r-${1}ow_${OAR_JOB_ID} ;
-    scp root@$HOST:/root/tts.log logs/$HHOSTNAME.$OAR_JOB_ID.log
-  ) &
+  ssh -o StrictHostKeyChecking=no root@$HOST "./greenfaas/g5k_deploy/run_text2speech.sh '$(IFS=','; echo "${SWIFT_HOSTS[*]}")' '$ITERATIONS' '$RUNS' >tts.log 2>&1 &" |& sed "s/^/    /"
   shift_left SWIFT_HOSTS
 done
 
@@ -55,7 +50,9 @@ echo "Waiting for tasks to finish..."
 while [ -n "$(tr -d ' ' <<<"${OW_HOSTS[@]}")" ]; do
   for HOST in "${OW_HOSTS[@]}"; do
     HHOSTNAME="$(cut -d '.' -f 1 <<<"$HOST")"
-    if [ -f "logs/$HHOSTNAME.$OAR_JOB_ID.log" ]; then
+    scp root@$HOST:/root/tts.log logs/$HHOSTNAME.$OAR_JOB_ID.log
+    if [[ "$(grep "Done" logs/$HHOSTNAME.$OAR_JOB_ID.log)" ]]; then
+      scp -r root@$HOST:/root/greenfaas/energy_results/$HHOSTNAME ./energy_results/${ITERATIONS}i-${RUNS}r-${1}ow_${OAR_JOB_ID} 2>/dev/null
       echo "  on $HOST: done"
       OW_HOSTS=("${OW_HOSTS[@]/$HOST}")
     fi
