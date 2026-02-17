99e061f00e6fcbf927041973e263dcbf24222938
Juloos <juloos.sfr@gmail.com>
2026-02-17 13:53:14 +0100

    debug 25

--- g5k_deploy/run_openwhisk.sh
+++ g5k_deploy/run_openwhisk.sh
@@ -17,8 +17,8 @@ helm uninstall owdev -n openwhisk
 kind delete cluster --name kind
 
 ./greenfaas/g5k_deploy/start-kind.sh &&
-  helm install owdev openwhisk/openwhisk -n openwhisk --create-namespace -f ./greenfaas/g5k_deploy/openwhisk_config.yml
-
-while true; do
-  kubectl get pods -n openwhisk --watch | grep --color=always -E "^owdev-install-packages-.*$|$"
-done
+  helm install owdev openwhisk/openwhisk -n openwhisk --create-namespace -f ./greenfaas/g5k_deploy/openwhisk_config.yml && {
+    while true; do
+      kubectl get pods -n openwhisk --watch | grep --color=always -E "^owdev-install-packages-.*$|$"
+    done
+  }
