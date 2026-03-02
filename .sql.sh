16d767c484a85677010b8844834c38aaba6b6954
Juloos <juloos.sfr@gmail.com>
2026-03-02 13:13:22 +0100

    refactor++

--- .sql.sh
+++ .sql.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export SQL0="core_count >= 12 AND memnode >= 32768 AND wattmeter=YES AND NOT cluster IN ('parasilo') AND (cluster!='paradoxe' OR host LIKE 'paradoxe-_.%' OR host LIKE 'paradoxe-1_.%' OR host LIKE 'paradoxe-2_.%' OR host LIKE 'paradoxe-30.%' OR host LIKE 'paradoxe-31.%' OR host LIKE 'paradoxe-32.%')"
-export SQL1="core_count >= 4 AND memnode >= 8192"
+export SQL_OW="core_count >= 12 AND memnode >= 32768 AND wattmeter=YES AND NOT cluster IN ('parasilo') AND (cluster!='paradoxe' OR host LIKE 'paradoxe-_.%' OR host LIKE 'paradoxe-1_.%' OR host LIKE 'paradoxe-2_.%' OR host LIKE 'paradoxe-30.%' OR host LIKE 'paradoxe-31.%' OR host LIKE 'paradoxe-32.%')"
+export SQL_SWIFT="core_count >= 4 AND memnode >= 8192"
