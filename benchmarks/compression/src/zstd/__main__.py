55b2bf06f9e6d441113d8842a3d64fa7b9722e2c
Juloos <juloos.sfr@gmail.com>
2026-03-18 16:59:25 +0100

    useless line

--- benchmarks/compression/src/zstd/__main__.py
+++ benchmarks/compression/src/zstd/__main__.py
@@ -82,7 +82,6 @@ def main(args):
             "pull" : (pull_end - pull_begin) / datetime.timedelta(seconds=1),
             "push" : (push_end - push_begin) / datetime.timedelta(seconds=1)
          },
-         "validation" : args.get("validation", {"process" : 0, "pull" : 0, "push" : 0}),
          "ipv4" : ipv4,
          "file" : file,
          "cid" : cid
