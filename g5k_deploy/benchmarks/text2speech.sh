9fe5c0df179e6b1ed9f4a4d8908032d7dcd9d769
Juloos <juloos.sfr@gmail.com>
2026-04-09 13:43:09 +0200

    do the same for tts

--- g5k_deploy/benchmarks/text2speech.sh
+++ g5k_deploy/benchmarks/text2speech.sh
@@ -33,9 +33,10 @@ BENCHARGS=("${@:5:$#}")
 
 echo "Initializing the Swift hosts..."
 for HOST in "${SWIFT_HOSTS[@]}"; do
-  echo "  on $HOST"
-  ssh $SSHFLAGS root@$HOST ./greenfaas/benchmarks/text2speech/init_swift.sh |& sed "s/^/    /"
+  HHOSTNAME="$(cut -d '.' -f 1 <<<"$HOST")"
+  ssh $SSHFLAGS root@$HOST ./greenfaas/benchmarks/compression/init_swift.sh |& sed "s/^/  $HHOSTNAME: /" &
 done
+wait
 
 echo "Deploying the benchmark..."
 for HOST in "${OW_HOSTS[@]}"; do
